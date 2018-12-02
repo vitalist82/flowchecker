6b27ac6ace651498e5fc26bd3a8435aa66f0e1e3
Tomas Beranek <Tomas.Beranek@massive.co>
2018-12-02 21:37:37 +0100

    removed Czech comment

--- FlowCheker/Program.cs
+++ FlowCheker/Program.cs
@@ -9,9 +9,6 @@ namespace FlowCheker
 {
     static class Program
     {
-        /// <summary>
-        /// Hlavní vstupní bod aplikace.
-        /// </summary>
         [STAThread]
         static void Main()
         {
